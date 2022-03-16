c187012b0a521e36f639c20aa6c156bc83acfb62
Dayane Chiozzi <daychiozzi@gmail.com>
2022-03-16 18:20:48 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return data.employees
+    .find((element) => element.firstName === employeeName || element.lastName === employeeName);
 }
+
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
