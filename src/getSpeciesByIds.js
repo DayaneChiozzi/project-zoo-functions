cff3ef1f28c0c6ed5b4bf5826ab6a6a38a583073
Dayane Chiozzi <daychiozzi@gmail.com>
2022-03-16 15:54:32 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
-const data = require('../data/zoo_data');
-
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const data = require("../data/zoo_data");
+function getSpeciesByIds(...id) {
+  const { species } = data;
+  return species.filter((element) => id.includes(element.id));
 }
+
 
 module.exports = getSpeciesByIds;
