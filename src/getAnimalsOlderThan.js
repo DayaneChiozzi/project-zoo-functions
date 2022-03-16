7db5836844fafd569fa4c8836ffdd294f0fb1c5b
Dayane Chiozzi <daychiozzi@gmail.com>
2022-03-16 17:23:12 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+// console.log(data);
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const resultSpecie = data.species.find((element) =>
+    animal.includes(element.name));
+
+  return resultSpecie.residents.every((element) => element.age >= age);
 }
+
+console.log(getAnimalsOlderThan('lions', 7));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
-const data = require("../data/zoo_data");
+const data = require('../data/zoo_data');
+
 function getSpeciesByIds(...id) {
   const { species } = data;
   return species.filter((element) => id.includes(element.id));
 }
-
 
 module.exports = getSpeciesByIds;
