8bad75645e51d080f469ba252a04e9a94eb89d72
Dayane Chiozzi <daychiozzi@gmail.com>
2022-03-17 19:08:14 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
 
+// Funcao esta retornando um objeto com chave: child, adult, senior
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    // filter esta verificando quais elementos do parametro entra na condicao e armazena nas chaves do obj
+    child: entrants.filter((element) => element.age < 18).length,
+    adult: entrants.filter((element) => element.age >= 18 && element.age < 50).length,
+    senior: entrants.filter((element) => element.age >= 50).length,
+  };
 }
-
+// funcao calcula o preco de entrada de acordo com o parametro recebido
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) { // a 1 condicao
+    return 0;
+  }
+
+  const { prices } = data; // desestruturacao do objeto
+  const resultCount = countEntrants(entrants); // constante esta recebendo a funcao acima
+  const totalPriceChild = prices.child * resultCount.child; // preco multiplica a qtd passada pela funcao acima
+  const totalPriceAdult = prices.adult * resultCount.adult; // preco multiplica a qtd passada pela funcao acima
+  const totalPriceSenior = prices.senior * resultCount.senior; // preco multiplica a qtd passada pela funcao acima
+  return totalPriceChild + totalPriceAdult + totalPriceSenior; // retorno é a soma das multiplicacoes
 }
 
 module.exports = { calculateEntry, countEntrants };
