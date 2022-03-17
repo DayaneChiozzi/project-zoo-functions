4d0f2e3cd96199f08979650245ae3eb1a3a69340
Dayane Chiozzi <daychiozzi@gmail.com>
2022-03-16 22:52:22 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const manager = isManager(managerId);
+  if (!manager) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const resultManager = data.employees.filter((element) => element.managers.includes(managerId));
+  const nameSubordinate = resultManager
+    .map((element) => `${element.firstName} ${element.lastName}`);
+  return nameSubordinate;
 }
 
 module.exports = { isManager, getRelatedEmployees };
