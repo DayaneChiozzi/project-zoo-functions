829e93d8b803df1d4ceeadfbd89946a8604c8c48
Dayane Chiozzi <daychiozzi@gmail.com>
2022-03-21 09:38:25 -0300

    excluido arquivo teste.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,12 +1,7 @@
 const data = require('../data/zoo_data');
-//mapear as especies
+
 function getAnimalMap(options) {
-  const {species} = data
-
- const mapear = species.map((element) => element === element.location)
- 
- console.log(mapear)
-
-console.log(getAnimalMap());
+  // seu código aqui
+}
 
 module.exports = getAnimalMap;
