6f299ea109f08961a202366e67e2c282bbf11712
Dayane Chiozzi <daychiozzi@gmail.com>
2022-03-21 11:00:45 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // const recebe o 1 elemento.id da const employees e compara o id paramentro
+  const employeesResult = employees.find((element) => element.id === id);
+  // const recebe o 1 elemento.id da const species e compara com a const acima, o 1 indice de responsibleFor
+  const animalFirst = species.find((element) => element.id === employeesResult.responsibleFor[0]);
+  // const recebe os residents da const acima e ordena do maior para o menor
+  const residentsSorted = animalFirst.residents.sort((a, b) => b.age - a.age);
+  // para acessar apenas o valor da const residentsSorted[1 indice] foi utilizado o obj.values
+  return Object.values(residentsSorted[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
